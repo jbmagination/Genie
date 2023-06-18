c02e1b15287664165438efd05e16f88ce031c1ec
JBMagination <jbmagination@jbmagination.com>
2023-06-18 09:05:25 -0700

    Update bagon-bagel.user.js

--- bagon-bagel/bagon-bagel.user.js
+++ bagon-bagel/bagon-bagel.user.js
@@ -22,7 +22,4 @@ if (!(params.has('bagon'))) {
   window.location.search = params.toString();
 }
 
-window.onload = () => {
-  document.querySelector('.react_song_page_preview_link').style = "display:none;";
-  document.querySelector('.OptOutButton__Container-e48zu0-0').style = "display:none;";
-}
+window.onload = () => document.querySelector('.react_song_page_preview_link').style = "display:none;";
